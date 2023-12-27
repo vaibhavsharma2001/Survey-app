f06180c272b971a313c1b53c25c47b0920515afb
vaibhavsharma2001 <136614875+vaibhavsharma2001@users.noreply.github.com>
2023-12-28 01:45:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+// app.get("*", (req, res) => {
+//   res.sendFile("build/index.html", { root: __dirname });
+// });
 
 const PORT = process.env.PORT || 5000;
 
