65e04a9cd8f3ab91a519eddc0a08adcbd2244a8a
vaibhavsharma2001 <136614875+vaibhavsharma2001@users.noreply.github.com>
2023-12-28 03:25:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const config = require("config");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://vaibhav1234:poiuytrewq@newcluster.dvgt1up.mongodb.net/?retryWrites=true&w=majority", {
+    await mongoose.connect("mongodb+srv://vaibhav1234:<password of your db>@newcluster.dvgt1up.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
