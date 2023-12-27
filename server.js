8c9d61f5cc2a11ce640ae247fcb14a9f16aee8ec
vaibhavsharma2001 <136614875+vaibhavsharma2001@users.noreply.github.com>
2023-12-28 01:06:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
+const path=require('path')
 const app = express();
 app.use(cors());
 
